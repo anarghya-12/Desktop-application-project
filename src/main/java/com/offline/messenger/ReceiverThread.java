72d7a1e02e308551cc88ba68fb88eb50efcf7934
anarghya-12 <166636310+anarghya-12@users.noreply.github.com>
2025-06-26 16:41:26 +0530

    Create ReceiverThread.java for recepient side

---
+++ src/main/java/com/offline/messenger/ReceiverThread.java
@@ -0,0 +1,30 @@
+package com.offline.messenger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ReceiverThread extends Thread {
+    private Socket socket;
+    private ChatFrame chatFrame;
+
+    public ReceiverThread(Socket socket, ChatFrame chatFrame) {
+        this.socket = socket;
+        this.chatFrame = chatFrame;
+    }
+
+    @Override
+    public void run() {
+        try (
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            String message;
+            while ((message = in.readLine()) != null) {
+                chatFrame.showMessage("Received: " + message);
+            }
+        } catch (IOException e) {
+            chatFrame.showMessage("Receiver disconnected: " + e.getMessage());
+        }
+    }
+}
